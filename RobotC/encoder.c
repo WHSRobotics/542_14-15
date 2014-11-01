7aa4c5c0fa8e30a322fc1bba5d361b6458c9e662
cardboardtheory <cardboardtheory@gmail.com>
2014-11-01 11:46:46 -0700

    tracking wheel encoder

--- RobotC/encoder.c
+++ RobotC/encoder.c
@@ -1,17 +1,26 @@
 #pragma config(Sensor, S1,     HTANG,               sensorI2CCustom)
-#include "C:\Program Files\Robomatter Inc\ROBOTC Development Environment\Includes\hitechnic-angle.h"
+#include "C:\Documents and Settings\Teemo Time\My Documents\Github\542_14-15\RobotC\drivers\hitechnic-angle.h"
+
+//cm
+float DIAMETER = 4.8;
+float ROBOT_RADIUS = 10; //arb
+float TW_X = 3; //arb
+float TW_Y = 3; //arb
 
 long getCirc(int DIAMETER)
 {
-	return(2*PI*DIAMETER);
+	return(PI*DIAMETER);
 }
 
-int CONVERSION = (5/3);
+//int CONVERSION = (5/3);
 task main()
 {
 	while(true) {
-	int initRotation = (HTANGreadAccumulatedAngle(HTANG)/360);
-	int rotation = (initRotation * CONVERSION);
-	rotation * getCirc(48);
+		/* int initRotation = (HTANGreadAccumulatedAngle(HTANG)/360);
+		int rotation = (initRotation * CONVERSION);
+		int dist = rotation * getCirc(4.8); */
+		float arcT = HTANGreadAccumulatedAngle(HTANG)/600.0 * DIAMETER * PI; //arc of tracking wheel (distance turned)
+		float dc = sqrt(pow(TW_X+ROBOT_RADIUS),2) + pow(TW_Y,2)); //radius between center and tracking wheel
+		float arcR = ((arcT*ROBOT_RADIUS)/dc);
 	}
 }
