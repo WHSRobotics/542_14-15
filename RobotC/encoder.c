3ba385061d30da52f6a615d44e6e258079392617
cardboardtheory <cardboardtheory@gmail.com>
2014-11-01 14:15:03 -0700

    encoder.c

--- RobotC/encoder.c
+++ RobotC/encoder.c
@@ -3,25 +3,20 @@
 
 //cm
 float DIAMETER = 4.8;
-float ROBOT_RADIUS = 10; //arbitrary
+float ROBOT_R = 10; //arbitrary robot radius
 float TW_X = 3; //arb
 float TW_Y = 3; //arb
 
-long getCirc(int DIAMETER)
-{
-	return(PI*DIAMETER);
-}
-
-//int CONVERSION = (5/3);
+//int RATIO = (5/3); //gear ratio 20/12
 task main()
 {
 	while(true) {
 		/* int initRotation = (HTANGreadAccumulatedAngle(HTANG)/360);
-		int rotation = (initRotation * CONVERSION);
-		int dist = rotation * getCirc(4.8); */
+		int rotation = (initRotation * RATIO);
+		int dist = rotation * getArc(4.8); */
 		float arcT = HTANGreadAccumulatedAngle(HTANG)/600.0 * DIAMETER * PI; //arc of tracking wheel (distance turned)
-		float x = TW_X + ROBOT_RADIUS;
+		float x = TW_X + ROBOT_R;
 		float dc = sqrt(pow(x,2) + pow(TW_Y,2)); //radius between center and tracking wheel
-		float arcR = ((arcT*ROBOT_RADIUS)/dc);
+		float arcR = ((arcT*ROBOT_R)/dc);
 	}
 }
