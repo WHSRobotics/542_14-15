f9c2d145e5c58e1b61d84c85fc1b1444308b8b22
bi0s0ldier <bi0s0ldier@users.noreply.github.com>
2014-12-01 16:30:38 -0800

    Update auto_tasks.h

--- RobotC/competition/auto_tasks.h
+++ RobotC/competition/auto_tasks.h
@@ -8,22 +8,19 @@ float distance = angleSensorval;
 
 void robotExpand()
 {
+	
 }
 
 void stopDrive()
 {
-	motor[LF] = 0;
-	motor[LB] = 0;
-	motor[RF] = 0;
-	motor[RB] = 0;
+	motor[L] = 0;
+	motor[R] = 0;
 }
 
 void driveSetFor(int speedLeft, int speedRight, int timeSec)
 {
-	motor[LF] = speedLeft;
-	motor[LB] = speedLeft;
-	motor[RF] = speedRight;
-	motor[RB] = speedRight;
+	motor[L] = speedLeft;
+	motor[R] = speedRight;
 }
 
 void moveStraight(float distCm, int speed)
@@ -33,6 +30,8 @@ void moveStraight(float distCm, int speed)
 	}
 	else
 	{
+		motor[L] = speed;
+		motor[R] = speed;
 	}
 }
 
@@ -44,12 +43,26 @@ void move(float radOrDistCm, float radiusCm, int speed)
 {
 	if(radiusCm == 0)
 	{
+		motor[L] = speed;
+		motor[R] = -speed;
 	}
 	else if(abs(radiusCm) == halfWidth)
 	{
+		if(radiusCm <= 0 && radiusCm >= -halfWidth)
+		{
+			motor[L] = 0;
+			motor[R] = speed;
+		}
+		else if(radiusCm >= 0 && radiusCm <= halfWidth)
+		{
+			motor[L] = speed;
+			motor[R] = 0;
+		}
 	}
 	else
 	{
+		motor[L] = speed;
+		motor[R] = speed;
 	}
 }
 
