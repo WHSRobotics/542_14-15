d629e5e1d56c11f73f9ad701849d13af8c0325ae
bi0s0ldier <chocolateyay@gmail.com>
2014-12-05 17:23:18 -0800

    Teleop update
    changed the tubelift and goallift buttons in dccontrol

---
+++ RobotC/competition/teleop_tasks_daniel.h
@@ -0,0 +1,168 @@
+#ifndef TELEOP_TASKS.H;
+#define TELEOP_TASKS.H;
+
+//INCLUDES
+#include "all_globVars.h"
+#include "JoystickDriver.c"
+
+//FUNCTIONS
+float linMap(float rLower, float rUpper, float dLower, float dUpper, float dVal)
+{
+	return (dVal - dLower) * (rUpper - rLower) / (dUpper - dLower)+rLower;
+}
+
+float joyMap(int joyIn, int THRESH)
+{
+	return (abs(joyIn) > THRESH)
+	? (100.0/16384.0 * sgn(joyIn) * pow(joyIn,2))
+	: 0;
+}
+
+//TASKS
+task servoPlate()
+{
+	while(true)
+	{
+		getJoystickSettings(joystick);
+		if(joy1Btn(01)&&headUp)
+		{
+			if(!togglePlate)
+			{
+				plateOpen = !plateOpen;
+				clampDown = plateOpen
+				?true
+				:false;
+			}
+			togglePlate = true;
+		}
+		else
+		{
+			togglePlate = false;
+		}
+		switch(plateOpen)
+		{
+			case true:
+				servo[liftR] = 50;
+				servo[liftL] = 220;
+				servo[beltGuard] = 0;
+				servo[intake] = 200;
+			break;
+
+			case false:
+				servo[liftR] = 255;
+				servo[liftL] = 5;
+			break;
+		}
+		//
+		if(joy1Btn(03)&&plateOpen)
+		{
+			if(!toggleClamp)
+			{
+				clampDown = !clampDown;
+			}
+			toggleClamp = true;
+		}
+		else
+		{
+			toggleClamp = false;
+		}
+		switch(clampDown)
+		{
+			case true:
+				servo[clampL] = 0;
+				servo[clampR] = 255;
+			break;
+
+			case false:
+				servo[clampL] = 255;
+				servo[clampR] = 0;
+			break;
+		}
+	}
+}
+
+
+task servoPush()
+{
+	while(true)
+	{
+		getJoystickSettings(joystick);
+		if(plateOpen)
+		{
+			if (joy1Btn(02))
+			{
+				clampDown = !clampDown
+				?true
+				:clampDown;
+				servo[pushL] = 75;
+				wait10Msec(10);
+				servo[pushR] = 175;
+			}
+			else
+			{
+				servo[pushR] = 5;
+				wait10Msec(10);
+				servo[pushL] = 235;
+			}
+		}
+	}
+}
+
+//DC Stuff
+
+task DCControl()
+{
+	while(true)
+	{
+		getJoystickSettings(joystick);
+		motor[goalLift] = (joy1Btn(05))
+		? 100
+		: (joy1Btn(07))
+		? -100
+		: 0;
+		motor[runBelt] = (joy1Btn(06))
+		? 100
+		: (joy1Btn(08))
+		? -100
+		: 0;
+		headUp = true;
+		if(headUp)
+		{
+			servo[headL] = 20;
+			servo[headR] = 230;
+		}
+		if(joy1Btn(2))
+		{
+			while(nMotorEncoder[tubeLift] < 1120) // Values need to be changed
+			{
+				motor[tubeLift] = 100;
+			}
+		}
+		else
+		{
+			motor[tubeLift] = 0;
+		}
+	}
+}
+
+//tube tilt
+
+//expand order
+//head lift and tube lift simultaneously
+//raise goal lift a bit
+//open plate
+
+//backward drive
+//prioritization
+
+task drive()
+{
+	while(true)
+	{
+		getJoystickSettings(joystick);
+		motor[driveL] = joyMap(joystick.joy1_y1, JOY_THRESH);
+		motor[driveR] = joyMap(joystick.joy1_y2, JOY_THRESH);
+	}
+}
+
+#endif
