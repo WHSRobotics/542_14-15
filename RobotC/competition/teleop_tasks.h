992a332c92f666b63b22ffb6bb448f40367be559
bi0s0ldier <chocolateyay@gmail.com>
2014-12-12 22:29:33 -0800

    Teleop tasks comments
    comments for various lines of code

--- RobotC/competition/teleop_tasks.h
+++ RobotC/competition/teleop_tasks.h
@@ -26,34 +26,35 @@ int joyMapCurve(int joyIn, int THRESH)
 }
 
 //TASKS
+//////Servo Plate Task///////
 task servoPlate()
 {
 	while(true)
 	{
 		getJoystickSettings(joystick);
-		if(joy2Btn(03))
+		if(joy2Btn(03))																															//If button 3 is pressed follow through with the following command
 		{
-			if(!togglePlate)
+			if(!togglePlate)																													//If the boolean togglePlate is false then commit the following command
 			{
-				plateOpen = !plateOpen;
-				headUp = true;
-				clampDown = plateOpen
-				?true
-				:false;
+				plateOpen = !plateOpen;																									//plateOpen boolean is set to true
+				headUp = true;																													//headUp boolean is set to true
+				clampDown = plateOpen																										//clampDown boolean value is determined by plateOpen boolean value
+				?true																																		//If plateOpen is true, then clampDown is set to true
+				:false;																																	//Else, clampDown is set to false
 			}
-			togglePlate = true;
+			togglePlate = true;																												//togglePlate is set to true
 		}
 		else
 		{
-			togglePlate = false;
+			togglePlate = false;																											//togglePlate is set to false if the 2nd joystick button 3 is not pressed
 		}
 
 		//Edit
-		if(abs(2-joystick.joy2_TopHat) == 2)
+		if(abs(2-joystick.joy2_TopHat) == 2)																				//If the joystick d-pad is at the 2 value then commit the following command
 		{
-			if(!toggleAngle)
+			if(!toggleAngle)																													//If the toggleAngle value is false, then commit the following command
 			{
-				plateAngleState = ((plateAngleState + sgn(2-joystick.joy2_TopHat)) < 0)
+				plateAngleState = ((plateAngleState + sgn(2-joystick.joy2_TopHat)) < 0)	//
 				?plateAngleState
 				:plateAngleState + sgn(2-joystick.joy2_TopHat);
 			}
@@ -157,53 +158,56 @@ task DCControl()
 {
 	while(true)
 	{
-		getJoystickSettings(joystick);
-		motor[goalLift] = joy2Btn(06)
-		? 100
-		: joy2Btn(08)
-		? -75
-		: 0;
-		motor[runBelt] = (joy1Btn(06))
-		? 100
-		: (joy1Btn(08))
-		? -100
-		: 0;
-		if(headUp)
+		getJoystickSettings(joystick);																							//Get the joystick settings from include file JoystickDriver
+		motor[goalLift] = joy2Btn(06)																								//If the 2nd joystick button 6 is pressed commit following command
+		? 100																																				//Motor[goalLift] power will be set to 100
+		: joy2Btn(08)																																//Else if the 2nd joystick button 8 is pressed commit following command
+		? -75																																				//motor[goalLift] power will be set to -75
+		: 0;																																				//Else the motor[goalLift] power will be set to 0
+		motor[runBelt] = (joy1Btn(06))																							//If the 1st joystick button 6 is pressed commit following command
+		? 100																																				//Motor[runBelt] power will be set to 100
+		: (joy1Btn(08))																															//Else if the 1st joystick button 8 is pressed commit following command
+		? -100																																			//motor[goalLift] power will be set to -100
+		: 0;																																				//Else the motor[goalLift] power will be set to 0
+		if(headUp)																																	//If headUp is true then commit following command
 		{
-			servo[headL] = 20;
-			servo[headR] = 230;
+			servo[headL] = 20;																												//servo[headL] is set to 20
+			servo[headR] = 230;																												//servo[headR] is set to 230
 		}
-		if(joy1Btn(02)&&joy2Btn(02))
+		if(joy1Btn(02)&&joy2Btn(02))																								//If both joysticks press their button 2 then commit the following command
 		{
-			if(abs(nMotorEncoder[tubeLift]) < (5.0 * motorEncoderRot))
+			if(abs(nMotorEncoder[tubeLift]) < (5.0 * motorEncoderRot))								//If the absolute value of the motorEncoder[tubeLift] is less than 5600 commit following command
 			{
-				motor[tubeLift] = 75;
+				motor[tubeLift] = 75;																										//Have motor[tubeLift] set to power 75
 			}
 		}
 		else
 		{
-			motor[tubeLift] = 0;
+			motor[tubeLift] = 0;																											//Else motor[tubeLift] is set to power 0
 		}
 	}
 }
-
+////////checkActive///////////
+//It's purpoes is to//////////
+//determine which joystick////
+//to be given priority to/////
 void checkActive(int THRESH)
 {
-	if((abs(joystick.joy1_y1) > THRESH) || (abs(joystick.joy1_y2) > THRESH))
+	if((abs(joystick.joy1_y1) > THRESH) || (abs(joystick.joy1_y2) > THRESH))			//If the absolute value of the 1st joystick sticks go past a threshold, commit the following command
 	{
-		joy1Active = true;
+		joy1Active = true;																													//1st joystick is active
 	}
 	else
 	{
-		joy1Active = false;
+		joy1Active = false;																													//Else the 1st joystick is not active
 	}
-	if((abs(joystick.joy2_y1) > THRESH) || (abs(joystick.joy2_y2) > THRESH))
+	if((abs(joystick.joy2_y1) > THRESH) || (abs(joystick.joy2_y2) > THRESH))			//If the absolute value of the 2nd joystick sticks go past a threshold, commit the following command
 	{
-		joy2Active = true;
+		joy2Active = true;																													//2nd joystick is active
 	}
 	else
 	{
-		joy2Active = false;
+		joy2Active = false;																													//Else the 2nd joystick is not active
 	}
 }
 
@@ -211,17 +215,17 @@ task drive()
 {
 	while(true)
 	{
-		if(joy1Btn(05))
+		if(joy1Btn(05))																															//If the 1st joystick button 5 is pressed, commit the following command
 		{
-			if(!toggleSlo)
+			if(!toggleSlo)																														//If the toggleSlo is false, then commit the following command
 			{
-				sloMo = !sloMo;
+				sloMo = !sloMo;																													//sloMo is set to true
 			}
-			toggleSlo = true;
+			toggleSlo = true;																													//toggleSlo is set to true
 		}
 		else
 		{
-			toggleSlo = false;
+			toggleSlo = false;																												//toggleSlo is set to false
 		}
 		getJoystickSettings(joystick);
 		checkActive(JOY_THRESH);
